5d697a281943f2ac356af35c5850b4e0a299036e
Kana Sabaratnam <37363346+kanacb@users.noreply.github.com>
2024-01-12 09:46:57 +0800

    Update admin_screen.dart

--- lib/screens/admin/admin_screen.dart
+++ lib/screens/admin/admin_screen.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:vx_index/businesses/businessesScreen.dart';
-import 'package:vx_index/categories/categoriesScreen.dart';
-import 'package:vx_index/commodities/commoditiesScreen.dart';
-import 'package:vx_index/messages/messages_screen.dart';
-import 'package:vx_index/screens/widgets/nav_bar.dart';
-import 'package:vx_index/screens/widgets/tab_bar.dart';
-import 'package:vx_index/users/userModel.dart';
-import 'package:vx_index/users/usersScreen.dart';
-
-import '../../currencies/currenciesScreen.dart';
+import '../../components/users/users.dart';
+import '../../components/users/usersMain.dart';
 import '../../global.dart';
+import '../../messages/messages_screen.dart';
+import '../widgets/nav_bar.dart';
+import '../widgets/tab_bar.dart';
 
 class AdminScreen extends StatefulWidget {
   const AdminScreen({super.key, required this.user});
-  final User user;
+  final Users user;
 
   @override
   State<AdminScreen> createState() => _AdminScreenState();
@@ -28,55 +23,9 @@ class _AdminScreenState extends State<AdminScreen> {
     'https://cdn.pixabay.com/photo/2014/09/08/17/32/humming-bird-439364_960_720.jpg',
     'https://cdn.pixabay.com/photo/2018/05/03/22/34/lion-3372720_960_720.jpg',
     'https://cdn.pixabay.com/photo/2021/06/01/07/03/sparrow-6300790_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2017/10/20/10/58/elephant-2870777_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2014/09/08/17/32/humming-bird-439364_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2018/05/03/22/34/lion-3372720_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2021/06/01/07/03/sparrow-6300790_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2017/10/20/10/58/elephant-2870777_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2014/09/08/17/32/humming-bird-439364_960_720.jpg',
-    'https://cdn.pixabay.com/photo/2018/05/03/22/34/lion-3372720_960_720.jpg'
   ];
   List<Map<String, dynamic>> title = [
     {'name': 'Users', 'object': 'users', "screen": const UsersScreen()},
-    {
-      'name': 'Businesses',
-      'object': 'businesses',
-      "screen": const BusinessesScreen()
-    },
-    {
-      'name': 'Currencies',
-      'object': 'currencies',
-      "screen": const CurrenciesScreen()
-    },
-    {
-      'name': 'Categories',
-      'object': 'categories',
-      "screen": const CategoriesScreen()
-    },
-    {
-      'name': 'Commodities',
-      'object': 'commodities',
-      "screen": const CommoditiesScreen()
-    },
-    {
-      'name': 'Delivery methods',
-      'object': 'deliverymethods',
-      "screen": const UsersScreen()
-    },
-    {'name': 'Inboxes', 'object': 'inboxes', "screen": const UsersScreen()},
-    {
-      'name': 'Payment terms',
-      'object': 'paymentterms',
-      "screen": const UsersScreen()
-    },
-    {'name': 'Prices', 'object': 'categories', "screen": const UsersScreen()},
-    {'name': 'Quotations', 'object': 'users', "screen": const UsersScreen()},
-    {
-      'name': 'request4quotes',
-      'object': 'request4quotes',
-      "screen": const UsersScreen()
-    },
-    {'name': 'won', 'object': 'rfqwons', "screen": const UsersScreen()},
   ];
 
   void _onItemTapped(int index) {

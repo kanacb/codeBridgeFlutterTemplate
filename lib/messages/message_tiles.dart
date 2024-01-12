16d75e544694ba1a55d89e48558ca8e8c498596e
Kana Sabaratnam <37363346+kanacb@users.noreply.github.com>
2024-01-12 08:22:36 +0800

    Update message_tiles.dart

--- lib/messages/message_tiles.dart
+++ lib/messages/message_tiles.dart
@@ -1,4 +1,4 @@
-import 'package:vx_index/users/userModel.dart';
+import '../components/users/users.dart';
 import 'package:flutter/material.dart';
 
 class ReceiverMessageTile extends StatelessWidget {

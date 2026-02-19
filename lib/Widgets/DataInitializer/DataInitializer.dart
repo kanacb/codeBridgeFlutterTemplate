dba5c5b03eec05c7b1cb58439f8bf9b4d7094c92
Kana Sabaratnam <37363346+kanacb@users.noreply.github.com>
2026-02-19 18:26:51 +0800

    Clean up DataInitializer by removing unused providers
    Removed unused provider imports and mappings from DataInitializer.

--- lib/Widgets/DataInitializer/DataInitializer.dart
+++ lib/Widgets/DataInitializer/DataInitializer.dart
@@ -1,18 +1,6 @@
-import 'package:aims/Widgets/IncomingMachineChecklists/IncomingMachineChecklistsProvider.dart';
-import 'package:aims/Widgets/IncomingMachineChecks/IncomingMachineChecksProvider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
-import 'package:rename/platform_file_editors/abs_platform_file_editor.dart';
-
-import '../AtlasChecklists/AtlasChecklistsProvider.dart';
-import '../AtlasChecks/AtlasChecksProvider.dart';
-import '../MemChecks/MemChecksProvider.dart';
-import '../MemMachineChecklists/MemChecklistsProvider.dart';
-import '../VendingMachine/VendingMachineProvider.dart';
 import '../Companies/CompanyProvider.dart';
-import '../ExternalChecklists/ExternalChecklistsProvider.dart';
-import '../ExternalChecks/ExternalChecksProvider.dart';
-import '../AtlasTickets/AtlasTicketProvider.dart';
 import 'DataFetchable.dart';
 
 class DataInitializer {
@@ -24,16 +12,6 @@ class DataInitializer {
   final Map<String, DataFetchable> Function(BuildContext) _providerMap =
       (ctx) => {
         'companies': Provider.of<CompanyProvider>(ctx, listen: false),
-        'vms': Provider.of<VendingMachineProvider>(ctx, listen: false),
-        'incomingMachineChecklists': Provider.of<IncomingMachineChecklistsProvider>(ctx, listen: false),
-        'incomingMachineChecks': Provider.of<IncomingMachineChecksProvider>(ctx, listen: false),
-        'atlasChecklists': Provider.of<AtlasChecklistsProvider>(ctx, listen: false),
-        'atlasChecks': Provider.of<AtlasChecksProvider>(ctx, listen: false),
-        'externalChecklists': Provider.of<ExternalChecklistsProvider>(ctx, listen: false),
-        'externalChecks': Provider.of<ExternalChecksProvider>(ctx, listen: false),
-        'memChecklists': Provider.of<MemChecklistsProvider>(ctx, listen: false),
-        'memChecks': Provider.of<MemChecksProvider>(ctx, listen: false),
-        'atlasTickets': Provider.of<AtlasTicketProvider>(ctx, listen: false),
         // Add more mappings here
       };
 
@@ -43,7 +21,6 @@ class DataInitializer {
 
     await Future.wait(
       registry.values.map((provider) {
-
         return provider.fetchAllAndSave();
       }),
     );

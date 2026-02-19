89c0f33cc6cc7e61b0546394129cdab8ec3d7b37
Kana Sabaratnam <kana@cloudbasha.com>
2026-02-20 05:57:46 +0800

    200226 4

--- lib/Utils/Services/Response.dart
+++ lib/Utils/Services/Response.dart
@@ -1,10 +1,11 @@
 class Response<T> {
   final T? data;
   final String? error;
+  final String? subClass;
   final int? statusCode;
   final String msg;
 
-  Response({required this.msg, this.error, this.statusCode, this.data});
+  Response({required this.msg, this.error, this.subClass,  this.statusCode, this.data});
 
   bool get isSuccess => error == null;
 }

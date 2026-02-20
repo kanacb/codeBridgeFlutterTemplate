f8e4d71a2f8ffc22918d963debb02f3428f0cc1c
Kana Sabaratnam <kana@cloudbasha.com>
2026-02-20 16:28:53 +0800

    200226 13

--- lib/Utils/HiveSetup.dart
+++ lib/Utils/HiveSetup.dart
@@ -67,49 +67,19 @@ class HiveSetup {
     // ~cb-add-service-adapters~
 
     // Open required boxes
-    if (!Hive.isBoxOpen('usersBox')) {
-      await Hive.openBox<User>('usersBox');
-    }
-    if (!Hive.isBoxOpen('profilesBox')) {
-      await Hive.openBox<Profile>('profilesBox');
-    }
-    if (!Hive.isBoxOpen('inboxesBox')) {
-      await Hive.openBox<Inbox>('inboxesBox');
-    }
-    if (!Hive.isBoxOpen('commentsBox')) {
-      await Hive.openBox<Comment>('commentsBox');
-    }
-    if (!Hive.isBoxOpen('userInvitesBox')) {
-      await Hive.openBox<UserInvite>('userInvitesBox');
-    }
-    if (!Hive.isBoxOpen('notificationsBox')) {
-      await Hive.openBox<CBNotification>('notificationsBox');
-    }
-    if (!Hive.isBoxOpen('documentsStorageBox')) {
-      await Hive.openBox<DocumentStorage>('documentsStorageBox');
-    }
-    if (!Hive.isBoxOpen('BranchesBox')) {
-      await Hive.openBox<Branches>('BranchesBox');
-    }
-    if (!Hive.isBoxOpen('CompaniesBox')) {
-      await Hive.openBox<Companies>('CompaniesBox');
-    }
-    if (!Hive.isBoxOpen('positionsBox')) {
-      await Hive.openBox<Positions>('positionsBox');
-    }
+
     // ~cb-add-hivebox~
   }
 
   List<SingleChildWidget> providers() {
     return [
-      ChangeNotifierProvider(create: (_) => UserProvider()),
       ChangeNotifierProvider(create: (_) => ProfileProvider()),
       ChangeNotifierProvider(create: (_) => InboxProvider()),
       ChangeNotifierProvider(create: (_) => CommentProvider()),
       ChangeNotifierProvider(create: (_) => UserInviteProvider()),
       ChangeNotifierProvider(create: (_) => NotificationProvider()),
       ChangeNotifierProvider(create: (_) => DocumentStorageProvider()),
-      ChangeNotifierProvider(create: (_) => PositionsProvider()),
+
       // ~cb-add-notifier~
     ];
   }

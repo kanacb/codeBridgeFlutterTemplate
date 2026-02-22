75a15e37a317450bbcc50c119ad4fa4210c50aef
Kana Sabaratnam <kana@cloudbasha.com>
2026-02-22 15:39:13 +0800

    firebase config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,12 +3,12 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.cb.aims.aims"
+    namespace = ""
     compileSdk = flutter.compileSdkVersion
-//    ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.cb.aims.aims"
+        applicationId = "com."
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -42,6 +42,8 @@ android {
 
     dependencies {
         coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:2.0.3")
+        implementation(platform("com.google.firebase:firebase-bom:34.9.0"))
+        implementation("com.google.firebase:firebase-analytics")
     }
 }
 
